230c8da794cc5eeb0c9fee0cf6094eacc4d92134
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-24 12:39:47 +0300

    changes the logic of LIFO implementation

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,21 +4,22 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """ LIFO cache policy """
     def __init__(self):
         super().__init__()
+        self.last_key = None
 
     def put(self, key, item):
         """Add item to cache"""
         if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 if key not in self.cache_data:
-                    # Get the first key
-                    first_key = next(iter(self.cache_data))
-                    self.cache_data.pop(first_key)
-                    print(f"DISCARD: {first_key}")
+                    if self.last_key:
+                        self.cache_data.pop(self.last_key)
+                        print(f"DISCARD: {self.last_key}")
             self.cache_data[key] = item
+            self.last_key = key
 
     def get(self, key):
         """Retrieve an item for the key from the cache"""
