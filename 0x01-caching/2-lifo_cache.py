37f85a436da5a4fb0cafcac3337f8d83be5f1876
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-24 12:15:05 +0300

    implements the LIFO policy

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Caching System """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ LIFO cache policy """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Add item to cache"""
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key not in self.cache_data:
+                    # Get the first key
+                    first_key = next(iter(self.cache_data))
+                    self.cache_data.pop(first_key)
+                    print(f"DISCARD: {first_key}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve an item for the key from the cache"""
+        return self.cache_data.get(key) if key else None
