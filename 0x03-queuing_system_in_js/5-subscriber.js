79073b8a7dab8136fc2ba7dc3cd9f3eb1b40fec3
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-27 10:47:45 +0300

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import { createClient } from "redis";
+
+// create a redis client
+const client = createClient();
+
+// Connect to the Redis Server
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// subscribe to the channel
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
