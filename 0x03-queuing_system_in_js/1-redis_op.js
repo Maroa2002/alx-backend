ce8f1739ae20b584f66198bc99e029bfc8c48ea2
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-27 08:42:20 +0300

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,37 @@
+import { createClient } from "redis";
+
+// create a redis client
+const client = createClient();
+
+// Connect to the Redis Server
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error(`Error setting value for ${schoolName}: ${err.message}`);
+    } else {
+      console.log('Reply:', reply);
+    }
+  });
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(`Error getting value for ${schoolName}: ${err.message}`);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
